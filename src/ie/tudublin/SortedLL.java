48c19989a04cb3e0050a6e34982878598ce66e3f
Adam O'Shea <C20372181@mytudublin.ie>
2022-03-03 21:52:51 +0000

    ll

---
+++ src/ie/tudublin/SortedLL.java
@@ -0,0 +1,109 @@
+// Sorted linked list with a sentinel node
+// Skeleton code
+import java.util.Scanner;
+
+class SortedLL
+{
+    // internal data structure and
+    // constructor code to be added here
+    class node 
+    {
+        int data;
+        Node next;
+
+        public node(int data) 
+        {
+            this.data = data;
+        }
+    }
+
+    Node head, z;
+
+    public SortedLL()
+    {
+        z = new Node(0);
+        z.next = z;
+        head = z;
+    }
+    
+    
+    // this is the crucial method
+    public void insert(int x)
+    {
+        Node prev, curr, t;
+        t = new Node(x);
+        curr = head;
+        prev = null;
+        z.data = x;
+        while (curr != null && curr.data < newNode.data) {
+            prev = curr;
+            curr = curr.next;
+        }
+        
+        ///
+        if (curr == head)
+        {
+            head = t;
+        }
+        else {
+            prev.next = t;
+        }
+        t.next = curr;
+    }    
+    
+    /*
+    public boolean remove(int x) {
+        Node prev, curr;
+        
+            }
+    */
+    public boolean isEmpty() {
+        if (head = z){
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public void display()
+    {
+        Node t = head;
+        System.out.print("\nHead -> ");
+        while( t != z) {
+            System.out.print(t.data + " -> ");
+            t = t.next;
+        }
+        System.out.println("Z\n");
+    }
+    
+    public static void main(String args[])   
+    {
+        SortedLL list = new SortedLL();
+        list.display();
+        
+        double x;
+        int i, r;
+        
+        
+           for(i = 1; i <= 5; ++i)  {
+           x =  (Math.random()*100.0);
+           r = (int) x; 
+           System.out.println("Inserting " + r);
+           list.insert(r);
+           list.display();            
+        }
+        
+        
+        /*
+        while(!list.isEmpty())  {
+            System.out.println("\nInput a value to remove: ");
+            Scanner in = new Scanner(System.in);
+            r = in.nextInt();
+            if(list.remove(r)) {
+                System.out.println("\nSuccessfully removed: " + r);
+            list.display(); }
+            else System.out.println("\nValue not in list");                        
+        }
+        */
+    }
+}
